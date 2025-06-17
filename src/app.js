abc2987da463bdcdc5fd05e435f73615ba9e4a09
Ayush <ayushempire03@gmail.com>
2025-06-17 22:09:19 +0530

    implemented temporary registration with firebase

--- src/app.js
+++ src/app.js
@@ -16,4 +16,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+import admin from "firebase-admin";
+import { ApiResponse } from "./utils/ApiResponse.js";
+import { ApiError } from "./utils/ApiError.js";
+admin.initializeApp({
+  credential: admin.credential.cert(
+    "./bhatkanti-users-firebase-adminsdk-fbsvc-eb56a52497.json"
+  ),
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userResponce = await admin.auth().createUser({
+    email: email,
+    password: password,
+    emailVerified: false,
+    disabled: false,
+  });
+
+  if (!userResponce) {
+    throw new ApiError(400, "error");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "user created", userResponce));
+});
+
 export { app };
